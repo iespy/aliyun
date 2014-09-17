8b61b0c133513e7d729730a27bd61c846937ca64
Mengnan Feng <y7@wlht.cn>
2014-09-17 14:39:22 +0800

    aliyun packagist

---
+++ sdk/aliyun/request/Ecs20140526DescribeAutoSnapshotPolicyRequest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * TOP API: ecs.aliyuncs.com.DescribeAutoSnapshotPolicy.2014-05-26 request
+ * 
+ * @author auto create
+ * @since 1.0, 2014-09-01 12:47:26
+ */
+class Ecs20140526DescribeAutoSnapshotPolicyRequest
+{
+	
+	private $apiParas = array();
+	
+	public function getApiMethodName()
+	{
+		return "ecs.aliyuncs.com.DescribeAutoSnapshotPolicy.2014-05-26";
+	}
+	
+	public function getApiParas()
+	{
+		return $this->apiParas;
+	}
+	
+	public function check()
+	{
+		
+	}
+	
+	public function putOtherTextParam($key, $value) {
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+}
